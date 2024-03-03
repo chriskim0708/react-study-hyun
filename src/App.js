dacc1f04827356a886634ebfab05d185268c083e
chriskim0708 <last1224@gmail.com>
2024-03-03 11:36:45 +0900

    20240303

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Post from "./Post";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Post />
     </div>
   );
 }
 
 export default App;
+
+/**
+ * 중첩된 컴포넌트 만들기
+ * 4분할된 컴포넌트 만들기
+ * 각각 분할된 화면을 2분할로 다시 컴포넌트 만들기
+ */
---
+++ src/Post.js
@@ -0,0 +1,11 @@
+import Post2 from "./Post2";
+import "./Post.css";
+
+export default function Post() {
+  return (
+    <div className="Post">
+      post
+      <Post2 />
+    </div>
+  );
+}
